8f928bd1994f332bb0b5bbd8c4738d985af4f6b9
Emily Rose <emily@contactvibe.com>
2012-11-30 05:52:23 -0800

    Attaching log instance to app. Loading serial module for arduino.

--- client.js
+++ client.js
@@ -2,14 +2,30 @@ var
 	fs = require('fs')
 	, path = require('path')
 	, util = require('util')
-	, stream = require('stream')
+	, events = require('events')
 	, argv = require(path.resolve(__dirname, 'app', 'argv'))
-	, ninja = require(path.resolve(__dirname, 'app', 'client'))
-	, config = require(path.resolve(__dirname, 'app', 'config'))
+	, client = require(path.resolve(__dirname, 'app', 'client'))
+	// , config = require(path.resolve(__dirname, 'app', 'config'))
 	, logger = require(path.resolve(__dirname, 'lib', 'logger'))
-	, log = new logger(argv).log
-	, client = new ninja(argv)
+	, app = new events.EventEmitter()
+	, log = new logger(argv)
+	, creds = {}
 ;
 
 logger.default = log;
+app.log = log;
 
+var ninja = new client(argv, creds, app);
+/**
+ * Note about apps (event emitters):
+ * 
+ * We can instantiate multiple apps to
+ * allow our modules to be namespaced/sandboxed
+ * if we so desire. This allows us to provide 
+ * isolation without any additional infrastructure
+ */
+
+if(argv.device && ninja) {
+
+	ninja.loadModule('serial', { device : argv.device, id : 'arduino' }, app);
+}
