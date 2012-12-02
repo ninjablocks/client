b28b6560049256c95393ab6ada1f403d75a5b313
Emily Rose <emily@contactvibe.com>
2012-12-02 14:36:25 -0800

    Added embedded rgbled to app/config

--- app/config.js
+++ app/config.js
@@ -6,7 +6,19 @@ module.exports = function config(ninja, app) {
 
 	if((ninja) && ninja.opts.device) {
 
-		ninja.loadModule('serial', { device : ninja.opts.device, id : 'arduino' }, app);
+		ninja.loadModule(
+
+			'serial'
+			, { device : ninja.opts.device, id : 'arduino' }
+			, app
+		);
+
+		ninja.loadModule(
+
+			'embedded'
+			, { components : [ 'rgbled' ] }
+			, app
+		);
 
 	}
 	
