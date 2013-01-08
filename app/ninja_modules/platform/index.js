1dddffc7445ac8e202f326acae6c4790c2504dc2
Emily Rose <emily@contactvibe.com>
2013-01-08 07:23:18 -0800

    Added platform deviceHandler, dataEvent, sendData methods. WIP

--- app/ninja_modules/platform/index.js
+++ app/ninja_modules/platform/index.js
@@ -5,6 +5,8 @@
 function platform(opts, app) {
 	
 	this.app = app;
+	this.log = app.log;
+	this.id = opts.id;
 
 	this.newDevice = function newDevice(dev) {
 
@@ -21,11 +23,94 @@ function platform(opts, app) {
 
 	this.onData = function onData(data) {
 
-		var json = 
+		var 
+			json = this.getJSON(data)
+			, type = Object.keys(json)[0] || undefined
+		;
+
+		if(!json || !type) {
+
+			this.log.debug("Ignoring unexpected JSON (%s)", json);
+			return;
+		}
+
+		this.dataEvent(type, json);
 	};
 
 	app.on('serial::new', this.newDevice);
 
+};
+
+platform.prototype.dataEvent = function dataEvent(type, data) {
+
+	var trigger = {
+
+		'ACK' : function(data) {
+
+			this.ackHandler(data.ACK || null);
+		}
+		, 'DEVICE' : function(data) {
+
+			this.deviceHandler(data.DEVICE || null);
+			this.log.debug("Device data: %s", data.DEVICE);
+		}
+		, 'PLUGIN' : function(data) {
+
+			this.log.debug("Device plugin: %s", data.PLUGIN);
+		}
+		, 'UNPLUG' : function(data) {
+
+			this.log.debug("Device unplug: %s", data.UNPLUG);
+		}
+		, 'ERROR' : function(data) {
+
+			this.log.debug("Device error: %s", data);
+		}
+	}
+
+	if(!trigger[type]) {
+
+		this.log.debug("Unrecognized data event %s", type);
+		return;
+	}
+
+	trigger[type](data);
+};
+
+platform.prototype.deviceHandler = function(dataset) {
+	
+	/**
+	 * device specific data handlers
+	 */
+	var handlers = {
+
+		2 : this.accelerometerData
+	};
+
+	if(!(dataset instanceof Array)) { return; }
+
+	dataset.map(function(device) {
+
+		if(handlers[device.D]) {
+
+			handlers[device.D](device);
+		}
+		else {
+
+			this.sendData(device);
+		}
+	});	
+};
+
+platform.prototype.accelerometerData = function(data) {
+	
+};
+
+platform.prototype.sendData = function(data) {
+	
+	if(!data) { return; }
+
+	app.emit('device::data', data);
 };
 
 platform.prototype.getJSON = function getJSON(data) {
@@ -36,8 +121,9 @@ platform.prototype.getJSON = function getJSON(data) {
 	}	
 	catch (e) {
 
-		this.app.debug("Ignoring invalid JSON (%s)", data);
+		this.log.debug("Ignoring invalid JSON (%s)", data);
 	}
+	return null;
 };
 
 module.exports = platform;
