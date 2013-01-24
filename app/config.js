5c256d8bee109ac5bc56e2dd6bacbe26576afd75
Emily Rose <emily@contactvibe.com>
2013-01-23 21:41:06 -0800

    Added ninja_module directory loader.

--- app/config.js
+++ app/config.js
@@ -1,3 +1,9 @@
+var 
+	mkdirp = require('mkdirp')
+	, path = require('path')
+	, fs = require('fs')
+;
+
 /**
  * TODO: configuration profiles
  */
@@ -9,16 +15,63 @@ module.exports = function config(ninja, app) {
 
 		return false;
 	}
-	
 	// default arduino device path	
 	if(!ninja.opts.client || ninja.opts.client == 'beagle') {
 
 		ninja.opts.device = '/dev/ttyO1';
 	}
+
+	loadPlatform(ninja, app); // embedded arduino
 	
-	/**
-	 * Load ninja cape & arduino modules if present
-	 */
+	var 
+		modPath = path.resolve(process.cwd(), 'ninja_modules')
+		, read = function read() {
+
+			fs.readdir(modPath, load);
+		}
+		, load = function load(err, mods) {
+
+			if(err) {
+
+				return app.log.error("config: Error loading modules (%s)", err);
+			}
+
+			mods.map(function(mod) {
+
+				ninja.loadModule(
+
+					mod
+					, config(mod)
+					, app
+				);
+			});
+		}
+		, config = function config(mod) {
+
+			/**		
+			 * get config file for module
+			 * either config/<mod>/config.json
+			 * or (default) config parameter from its package.json
+			 */
+			 return { };
+		}
+	;
+
+	mkdirp(modPath, read);
+
+	ninja.emit('loaded'); // done loading modules
+	ninja.connect();
+
+	return ninja;
+};
+
+/**
+ * Load ninja cape & arduino modules if present
+ * at the moment these modules rely on a proper load order
+ * TODO: fix this so they can load asynchronously.
+ */
+function loadPlatform(ninja, app) {
+
 	if(ninja.opts.device) {
 
 		// arduino controller
@@ -36,17 +89,5 @@ module.exports = function config(ninja, app) {
 			, { device : ninja.opts.device, id : 'arduino' }
 			, app
 		);
-
-		ninja.loadModule(
-
-			'network'
-			, { }
-			, app
-		);
 	}
-	
-	ninja.emit('loaded'); // done loading modules
-	ninja.connect();
-
-	return ninja;
 };
