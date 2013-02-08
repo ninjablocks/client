f52c885e9b9e5c2811c402f2adae961fc58c3d18
Emily Rose <emily@contactvibe.com>
2013-02-08 05:22:07 -0800

    Platform now streaming serial or net devices.

--- ninja_modules/platform/index.js
+++ ninja_modules/platform/index.js
@@ -3,78 +3,128 @@
  */
 
 var 
-	stream = require('stream')
+	serialport = require('serialport')
+	, stream = require('stream')
 	, util = require('util')
+	, path = require('path')
+	, net = require('net')
+	, fs = require('fs')
 ;
 
+/**
+ * platform.device = serial / net stream to device data (JSON stream)
+ * 
+ */
 function platform(opts, app) {
 	
+	stream.call(this);
+
 	this.app = app;
 	this.log = app.log;
+	this.device = undefined;
 
-	this.newDevice = function newDevice(dev) {
+	if((!opts.devicePath) && opts.env == "production") {
 
-		if(dev.id != 'arduino') { return; }
+		opts.devicePath = "/dev/ttyO1";
+	}
+	// don't bother if neither are specified
+	if(!opts.devicePath && !opts.deviceHost) {
 
-		app.log.debug("Initializing arduino...");
+		return this.log.info("platform: No device path or host specified");
+	}
 
-		dev.on('open', function() {
+	if(opts.deviceHost) {
+		
+		var str = this.createNetStream(opts.deviceHost, opts.devicePort);
+	}
 
-			console.log("Opened.");
-		});
-		dev.on('close', function() {
+	else {
 
-			console.log("Closed.");
-		});
-		dev.on('error', function(err) {
+		if(!checkPath(opts.devicePath)) { 
 
-			console.log("Error. %s", err);
-		});
-		dev.on('data', function(dat) {
+			return this.log.error(
 
-			// console.log("Data: %s", dat);
-		});
-
-		dev.connect();
-	};
-
-	this.onData = function onData(data) {
-
-		var 
-			json = this.getJSON(data)
-			, type = Object.keys(json)[0] || undefined
-		;
-
-		if(!json || !type) {
-
-			this.log.debug("Ignoring unexpected JSON (%s)", json);
-			return;
+				"platform: Device path doesn't exist? (%s)"
+				, opts.devicePath
+			);
 		}
+	
+		var str = this.createSerialStream(opts.devicePath);
+	}
 
-		this.dataEvent(type, json);
+	if(!str) {
+		
+		this.log.error("platform: Error creating device stream");
+	}
+	function checkPath(path) {
+
+		var exists = fs.existsSync || path.existsSync;
+		return exists(path);
 	};
+};
 
-	stream.call(this);
-	app.on('serial::new', this.newDevice);
+util.inherits(platform, stream);
 
+platform.prototype.createNetStream = function createNetStream(host, port) {
+
+	var mod = this;
+
+	if(!host) { return false; }
+	if(!port) { port = 9000; } // default!
+
+	mod.deviceHost = host;
+	mod.devicePort = port;
+	mod.device = net.connect(port, host, connectHandler);
+	console.log("host: %s, port: %s", host, port)
+	mod.device.on('error', mod.onError.bind(mod));
+	mod.device.on('close', mod.onClose.bind(mod));
+	mod.log.debug(
+
+		"platform: Opening net connection (%s:%s)"
+		, mod.deviceHost
+		, mod.devicePort
+	);
+	function connectHandler() {
+
+		mod.log.info("platform: Net connection established");
+	}
+	return mod.device;
+};
+
+platform.prototype.createSerialStream = function createSerialStream(path) {
+
+	var mod = this;
+	if(!path) { return false; }
+	mod.devicePath = path;
+	mod.device = new serialport.SerialPort(opts.devicePath, {
+
+		parser : serialport.parsers.readline("\n")
+		, baudrate : 115200
+	});
+	mod.device.on('error', mod.onError.bind(mod));
+	mod.device.on('close', mod.onClose.bind(mod));
+	mod.log.debug(
+
+		"platform: Opening serial connection (%s)"
+		, mod.devicePath
+	);
+	return mod.device;
 };
 
 platform.prototype.onOpen = function onOpen() {
 
-	this.log.debug("Arduino initialized");
+	this.log.debug("platform: Connection established");
 };
 
 platform.prototype.onClose = function onClose() {
 
-	this.log.debug("Arduino disconnected");
+	this.log.debug("platform: Connection closed");
 };
 
 platform.prototype.onError = function onError(err) {
 
-	this.log.error("Arduino error: %s", err);
+	this.log.error("platform: %s", err);
 };
-
-util.inherits(platform, stream);
 
 platform.prototype.dataEvent = function dataEvent(type, data) {
 
