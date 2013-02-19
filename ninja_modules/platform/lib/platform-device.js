de70c9e7acf51290d9bd452680615fb039a25c52
Emily Rose <emily@contactvibe.com>
2013-02-19 04:59:03 -0800

    Added tiny generic device stream for platform devices.

---
+++ ninja_modules/platform/lib/platform-device.js
@@ -0,0 +1,21 @@
+var
+	stream = require('stream')
+	, util = require('util')
+;
+
+module.exports = device;
+
+function device(G, V, D) {
+	
+	
+	if(!D) { return false; }
+	
+	stream.call(this);
+
+	this.V = 0;
+	this.G = G || "0";
+	this.D = D || undefined;
+
+};
+
+util.inherits(device, stream);
