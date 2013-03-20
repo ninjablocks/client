d50b7af9b738c319f70fc54d8c190ae7fd7a3e31
Emily Rose <emily@contactvibe.com>
2013-03-20 15:03:23 -0700

    Added app/client/updater.js

---
+++ app/client/updater.js
@@ -0,0 +1,55 @@
+var
+	exec = require('child_process').exec
+	, path = require('path')
+	//, node_updater = '/opt/utilities/ninja_update_node'
+	, node_updater = './updater.js'
+	, otherTimeout = 2000 //timeout in ms for non-node updates
+;
+
+module.exports = updater;
+function updater(client) {
+	
+	client.prototype.updateHandler = function updateHandler(updateList) {
+
+		var mod = this;
+		updateList.map(updateCheck);
+		function updateCheck(update) {
+
+			if(update == 'node') { // client update!
+
+				mod.app.emit('client::updating');
+				var opts = {
+
+					env : process.env
+				};
+				mod.log.info("client: Beginning update...");
+				exec(node_updater, opts, function(err, stdout, stderr) {
+
+					if(err) {
+
+						mod.log.error("client: %s", err);
+						return;
+					}
+					mod.log.info("client: Update complete. Restarting.");
+					mod.log.debug(
+
+						"client: Update utility output (%s): %s"
+						, path.basename(node_updater)
+						, stdout
+					);
+					process.exit();
+				});
+			}
+			else { // non-node update, just restart.
+
+				mod.log.info(
+
+					"client: Update for %s. Restarting in %s seconds."
+					, update
+					, Math.round(otherTimeout / 1000)
+				);
+				setTimeout(process.exit, otherTimeout);
+			}
+		};
+	};
+};
