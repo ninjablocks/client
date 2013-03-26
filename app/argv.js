1598c0661301b6f451bf75a2cdfdcd27df38b75c
Daniel Friedman <danielnfriedman@gmail.com>
2013-03-26 14:28:05 +1100

    Default client name to NINJA_CLIENT_NAME

--- app/argv.js
+++ app/argv.js
@@ -31,6 +31,7 @@ if (process.env.NODE_ENV === "development") {
 		, cloudPort : 3001
 		, apiPort		: 3000
 		, secure : false
+		, client : process.env.NINJA_CLIENT_NAME
 	}
 }
 else if (process.env.NODE_ENV === "hacking") {
@@ -47,6 +48,7 @@ else if (process.env.NODE_ENV === "hacking") {
 		, cloudPort : 443
 		, apiPort 	: 443
 		, secure : true
+		, client : process.env.NINJA_CLIENT_NAME
 	}
 }
 else {
@@ -64,6 +66,7 @@ else {
 		, cloudPort : 443
 		, apiPort		: 443
 		, secure : true
+		, client : process.env.NINJA_CLIENT_NAME
 	}
 	if(!process.env.NODE_ENV) { process.env.NODE_ENV = "production"; }
 }
