44e945e0a17ddc5028fe5ae60b63d8bf08cbc615
☃ Elliot Shepherd <elliot@ninjablocks.com>
2014-03-18 17:19:11 +1100

    Device outgoing data... moving things around for funsies.

--- index.js
+++ index.js
@@ -2,29 +2,48 @@
 var accio = require;
 
 var events = accio('events');
-var CloudConnection = accio('./lib2/CloudConnection');
-var Log = accio('./lib2/Log');
-var Credentials = accio('./lib2/Credentials');
-var DriverLoader = accio('./lib2/DriverLoader');
 var fs = accio('fs');
 var path = accio('path');
+
+var CloudConnection = accio('./lib/CloudConnection');
+var Log = accio('./lib/Log');
+var Credentials = accio('./lib/Credentials');
+var DriverLoader = accio('./lib/DriverLoader');
+var DeviceManager = accio('./lib/DeviceManager');
 
 var app = new events.EventEmitter();
 app.log = Log.getLogger('NB');
 
-var banner = fs.readFileSync('./lib2/banner', 'utf-8');
+var banner = fs.readFileSync('./lib/banner', 'utf-8');
 app.log.debug(banner);
 
-var opts = accio('./lib2/Options');
+
+// 1. Load our options
+var opts = accio('./lib/Options');
 app.opts = opts;
 
+// 2. Send any uncaught exceptions to our log... 
+// XXX: Nothing should be uncaught. Handle it with domains for the drivers.
 process.on('uncaughtException', function(err) {
   app.log.error('UNCAUGHT EXCEPTION', err);
 });
 
-var drivers = new DriverLoader(app, path.resolve(__dirname, 'config'), path.resolve(__dirname, 'drivers'));
-
+// 3. Set up our cloud connection
 var creds = new Credentials(opts, app);
 var cloud = new CloudConnection(opts, creds, app);
 
+// 4. Fire up the device manager
+var deviceManager = new DeviceManager(app, cloud);
+
+// 5. Load the drivers
+var drivers = new DriverLoader(app, path.resolve(__dirname, 'config'), path.resolve(__dirname, 'drivers'));
+
+// XXX: Move me.
+app.getGuid = function(device) {
+  return [
+    creds.serial, device.G, device.V, device.D
+  ].join('_');
+};
+
+// 6. Connect
 cloud.connect();
